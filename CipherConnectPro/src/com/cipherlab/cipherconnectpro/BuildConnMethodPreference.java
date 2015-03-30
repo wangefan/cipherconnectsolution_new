39d83b3dff0f1a916a0d91c3add7d674699ce20d
wangefan <wangefan@gmail.com>
2015-03-30 10:21:40 +0000

    [What]remove unnecessary code.

--- CipherConnectPro/src/com/cipherlab/cipherconnectpro/BuildConnMethodPreference.java
+++ CipherConnectPro/src/com/cipherlab/cipherconnectpro/BuildConnMethodPreference.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class BuildConnMethodPreference extends Preference 
