fe15431374e9dacfa0f8f40d97698f75d776a807
wangefan <wangefan@gmail.com>
2015-03-31 03:37:58 +0000

    [What] Refine NotificationUtil

--- CipherConnectPro/src/com/cipherlab/util/NotificationUtil.java
+++ CipherConnectPro/src/com/cipherlab/util/NotificationUtil.java
@@ -1,11 +1,13 @@
 package com.cipherlab.util;
 
 import android.R;
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 /**
  * @author william.lu
@@ -19,17 +21,22 @@ public class NotificationUtil {
     {
         //if (NOTIFY_ID == 0)
         //	NOTIFY_ID = System.identityHashCode(context);
-
         NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        
-        Notification msg = new Notification(icon, title, 0);
-        PendingIntent PI = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-        msg.setLatestEventInfo(context, title, message, PI);
-        mNotificationManager.notify(NOTIFY_ID, msg);
-
-        PI = null;
-        msg = null;
+        mNotificationManager.notify(NOTIFY_ID, GetNotificaion(icon, context, title, message, intent));
         mNotificationManager = null;
+    }
+    
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public static Notification GetNotificaion(int icon, Context context, String title, String message, Intent intent) 
+    {
+    	Notification.Builder notiBuilder= new Notification.Builder(context);
+		notiBuilder.setSmallIcon(icon);
+		notiBuilder.setContentTitle(title);
+		notiBuilder.setContentText(message);
+		notiBuilder.setWhen(System.currentTimeMillis());
+		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+		notiBuilder.setContentIntent(pendingIntent);
+		return notiBuilder.build(); 
     }
 
     public static void cancel(Context context) 
